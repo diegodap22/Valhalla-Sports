e0bed5226c6ed07bb7201a253e99d9dca2a26abf
diaparra <diegodap22@gmail.com>
2022-11-26 13:37:04 -0300

    first commit

---
+++ Tienda/tiendaDeportes/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class Producto(models.Model):
+    Nombre= models.CharField(max_length=50)
+    imagen=models.ImageField(upload_to="tiendaDeportes",null=True,blank=True)
+    precio=models.FloatField()
+    stock=models.IntegerField()
+    created=models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated=models.DateTimeField(auto_now_add=True, auto_now=False)
+
+class Meta:
+    verbose_name='Producto'
+    verbose_name_plural='Productos'
+
+def _stf_(self):
+    return self.Nombre
